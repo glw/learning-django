b0f4e8856eb6075d2a38b4d8a7ff8be9b3ae56bf
glw <garretlewis@gmail.com>
2021-10-07 13:05:02 -0500

    initial commit. basic django set up with a dynamic path and redirects.

---
+++ monthly_challenges/challenges/views.py
@@ -0,0 +1,37 @@
+from django.http.response import HttpResponseNotFound, HttpResponseRedirect
+from django.shortcuts import render
+from django.http import HttpResponse
+
+
+monthly_challenges = {
+    "january": "eat no meat",
+    "february": "walk for at least 20 min",
+    "march": "learn django for 20 min",
+    "april": "go for a run",
+    "may": "eat no meat",
+    "june": "walk for at least 20 min",
+    "july": "learn django for 20 min",
+    "august": "go for a run",
+    "september": "eat no meat",
+    "october": "walk for at least 20 min",
+    "november": "learn django for 20 min",
+    "december": "go for a run"
+}
+
+# Create your views here.
+
+def monthly_challenge_by_number(request, month):
+    months = list(monthly_challenges.keys())
+
+    if month > len(months):
+        return HttpResponseNotFound("Invalid month")
+        
+    redirect_month = months[month -1]
+    return HttpResponseRedirect("/challenges/" + redirect_month)
+
+def monthly_challenge(request, month):
+    try:
+        challenge_text = monthly_challenges[month]
+        return HttpResponse(challenge_text)
+    except:
+        return HttpResponseNotFound("This month is not supported")
