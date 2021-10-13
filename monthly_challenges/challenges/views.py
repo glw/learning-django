170654af01ff4e9cc8af9091165f98eada2de880
glw <garretlewis@gmail.com>
2021-10-13 00:09:09 -0500

    404 pages method 1. Added templates/404.html

--- monthly_challenges/challenges/views.py
+++ monthly_challenges/challenges/views.py
@@ -2,6 +2,7 @@ from django.http import response
 from django.http import HttpResponse, HttpResponseNotFound, HttpResponseRedirect
 from django.shortcuts import render
 from django.urls import reverse
+from django.template.loader import render_to_string
 
 
 monthly_challenges = {
@@ -48,4 +49,5 @@ def monthly_challenge(request, month):
             "month_name": month
         })
     except:
-        return HttpResponseNotFound("<h1>This month is not supported</h1>")
+        respoonse_data = render_to_string('404.html')
+        return HttpResponseNotFound(respoonse_data)
