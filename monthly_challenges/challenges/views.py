fd08fd9889bdc683145140a4fe57ede7992844e6
glw <garretlewis@gmail.com>
2021-10-24 21:00:21 -0500

    adding static files, adding global static files, adding css, and building static urls dynamically.

--- monthly_challenges/challenges/views.py
+++ monthly_challenges/challenges/views.py
@@ -1,6 +1,6 @@
-from django.http.response import HttpResponseNotFound, HttpResponseRedirect
+from django.http import Http404, HttpResponseNotFound, HttpResponseRedirect
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.urls import reverse
 
 
 monthly_challenges = {
@@ -15,23 +15,36 @@ monthly_challenges = {
     "september": "eat no meat",
     "october": "walk for at least 20 min",
     "november": "learn django for 20 min",
-    "december": "go for a run"
+    "december": None
 }
 
 # Create your views here.
 
+def index(request):
+    list_items = ''
+    months = list(monthly_challenges.keys())
+
+    return render(request, "challenges/index.html", {
+        "months": months
+    })
+
+        
 def monthly_challenge_by_number(request, month):
     months = list(monthly_challenges.keys())
 
     if month > len(months):
         return HttpResponseNotFound("Invalid month")
         
-    redirect_month = months[month -1]
-    return HttpResponseRedirect("/challenges/" + redirect_month)
+    redirect_month = months[month - 1]
+    redirect_path = reverse("month-challenge", args = [redirect_month]) # /challenge/january
+    return HttpResponseRedirect(redirect_path)
 
 def monthly_challenge(request, month):
     try:
         challenge_text = monthly_challenges[month]
-        return HttpResponse(challenge_text)
+        return render(request, "challenges/challenge.html", {
+            "text": challenge_text,
+            "month_name": month
+        })
     except:
-        return HttpResponseNotFound("This month is not supported")
+        raise Http404()
