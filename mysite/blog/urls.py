b52d49b951c550df4fdab0c866ff792b40d431b3
glw <garretlewis@gmail.com>
2021-10-25 00:30:38 -0500

    adding urls and views

---
+++ mysite/blog/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+
+from . import views
+
+urlpatterns = [
+    path("", views.start_page, name="start-page"),
+    path("posts", views.posts, name="posts-page"),
+    path("posts/<slug:slug>", views.post_detail, name="post-detail-page")
+]
